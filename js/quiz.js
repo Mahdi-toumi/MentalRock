abc19a5a555e8e1f6030839b171fd976addbac67
Mahdi-toumi <152063136+Mahdi-toumi@users.noreply.github.com>
2024-05-02 21:22:11 +0100

    now

--- js/quiz.js
+++ js/quiz.js
@@ -1,133 +1,133 @@
-const startButton = document.getElementById('start-btn')
-const nextButton = document.getElementById('next-btn')
-const backButton = document.getElementById('back-btn')
-const questionContainerElement = document.getElementById('question-container')
-const questionElement = document.getElementById('question')
-const answerButtonsElement = document.getElementById('answer-buttons')
+const startButton = document.getElementById('start-btn');
+const nextButton = document.getElementById('next-btn');
+const backButton = document.getElementById('back-btn');
+const questionContainerElement = document.getElementById('question-container');
+const questionElement = document.getElementById('question');
+const answerButtonsElement = document.getElementById('answer-buttons');
 
-let shuffledQuestions, currentQuestionIndex
+let shuffledQuestions, currentQuestionIndex;
 
-startButton.addEventListener('click', startGame)
+startButton.addEventListener('click', startGame);
 nextButton.addEventListener('click', () => {
-  currentQuestionIndex++
-  setNextQuestion()
-})
+  currentQuestionIndex++;
+  setNextQuestion();
+});
 
 function startGame() {
-  startButton.classList.add('hide')
-  shuffledQuestions = questions.sort(() => Math.random() - .5)
-  currentQuestionIndex = 0
-  questionContainerElement.classList.remove('hide')
-  setNextQuestion()
+  startButton.classList.add('hide');
+  shuffledQuestions = questions.sort(() => Math.random() - 0.5);
+  currentQuestionIndex = 0;
+  questionContainerElement.classList.remove('hide');
+  setNextQuestion();
 }
 
 function setNextQuestion() {
-  resetState()
-  showQuestion(shuffledQuestions[currentQuestionIndex])
+  resetState();
+  showQuestion(shuffledQuestions[currentQuestionIndex]);
 }
 
 function showQuestion(question) {
-  questionElement.innerText = question.question
+  questionElement.innerText = question.question;
   question.answers.forEach(answer => {
-    const button = document.createElement('button')
-    button.innerText = answer.text
-    button.classList.add('btn')
+    const button = document.createElement('button');
+    button.innerText = answer.text;
+    button.classList.add('btn');
     if (answer.correct) {
-      button.dataset.correct = answer.correct
+      button.dataset.correct = answer.correct;
     }
-    button.addEventListener('click', selectAnswer)
-    answerButtonsElement.appendChild(button)
-  })
+    button.addEventListener('click', selectAnswer);
+    answerButtonsElement.appendChild(button);
+  });
 }
 
 function resetState() {
-  clearStatusClass(document.body)
-  nextButton.classList.add('hide')
+  clearStatusClass(document.body);
+  nextButton.classList.add('hide');
   while (answerButtonsElement.firstChild) {
-    answerButtonsElement.removeChild(answerButtonsElement.firstChild)
+    answerButtonsElement.removeChild(answerButtonsElement.firstChild);
   }
 }
 
 function selectAnswer(e) {
-  const selectedButton = e.target
-  const correct = selectedButton.dataset.correct
-  setStatusClass(document.body, correct)
+  const selectedButton = e.target;
+  const correct = selectedButton.dataset.correct;
+  setStatusClass(document.body, correct);
   Array.from(answerButtonsElement.children).forEach(button => {
-    setStatusClass(button, button.dataset.correct)
-  })
+    setStatusClass(button, button.dataset.correct);
+  });
   if (shuffledQuestions.length > currentQuestionIndex + 1) {
-    nextButton.classList.remove('hide')
+    nextButton.classList.remove('hide');
   } else {
-    startButton.innerText = 'Restart' 
-    backButton.innerText = 'Back'
-    startButton.classList.remove('hide')
-    backButton.classList.remove('hide')
+    startButton.innerText = 'Restart'; 
+    backButton.innerText = 'Back';
+    startButton.classList.remove('hide');
+    backButton.classList.remove('hide');
   }
 }
 
 function setStatusClass(element, correct) {
-  clearStatusClass(element)
+  clearStatusClass(element);
   if (correct) {
-    element.classList.add('correct')
+    element.classList.add('correct');
   } else {
-    element.classList.add('wrong')
+    element.classList.add('wrong');
   }
 }
 
 function clearStatusClass(element) {
-  element.classList.remove('correct')
-  element.classList.remove('wrong')
+  element.classList.remove('correct');
+  element.classList.remove('wrong');
 }
 
 const questions = [
   {
-    question: 'Mental Health is',
+    question: 'La santé mentale est',
     answers: [
-      { text: 'Absence of Mental Disorder', correct: false },
-      { text: 'Important for specific people', correct: false },
-      { text: 'Cognitive, behavioral, and emotional well-being', correct: true },
-      { text: 'Not related to well being of a person', correct: false }
+      { text: 'Absence de trouble mental', correct: false },
+      { text: 'Importante pour des personnes spécifiques', correct: false },
+      { text: 'Le bien-être cognitif, comportemental et émotionnel', correct: true },
+      { text: 'Pas liée au bien-être d\'une personne', correct: false }
     ]
   },
   {
-    question: 'Having mental health conditions means:',
+    question: 'Avoir des troubles de santé mentale signifie :',
     answers: [
-      { text: 'Disturbances in a person\'s thinking, feeling, or behavior', correct: true },
-      { text: 'Low intelligence', correct: false },
-      { text: 'Sign of weakness', correct: false },
-      { text: 'Lack of Will-power', correct: false }
+      { text: 'Des perturbations dans la pensée, le sentiment ou le comportement d\'une personne', correct: true },
+      { text: 'Une intelligence faible', correct: false },
+      { text: 'Un signe de faiblesse', correct: false },
+      { text: 'Un manque de volonté', correct: false }
     ]
   },
   {
-    question: 'Mental Illness',
+    question: 'La maladie mentale',
     answers: [
-      { text: 'Can be treated! ', correct: true },
-      { text: 'Cannot be treated!', correct: false }
+      { text: 'Peut être traitée!', correct: true },
+      { text: 'Ne peut pas être traitée!', correct: false }
     ]
   },
   {
-    question: 'Poor mental health increases the risk for long-lasting (chronic) physical conditions like Heart Disease, Stroke & Cancer, etc.',
+    question: 'Une mauvaise santé mentale augmente le risque de conditions physiques durables (chroniques) telles que les maladies cardiaques, les accidents vasculaires cérébraux et le cancer, etc.',
     answers: [
-      { text: 'Yes', correct: true },
-      { text: 'No', correct: false }
+      { text: 'Oui', correct: true },
+      { text: 'Non', correct: false }
     ]
   },
   {
-    question: 'If you know someone of poor mental health:',
+    question: 'Si vous connaissez quelqu\'un avec une mauvaise santé mentale :',
     answers: [
-      { text: 'Reaching out and letting them know that help is available', correct: false },
-      { text: 'Helping them access mental health services', correct: false },
-      { text: 'Learning and sharing facts, clear out myths if you are aware', correct: false },
-      { text: 'All of the above', correct: true }
+      { text: 'Les contacter et leur faire savoir que de l\'aide est disponible', correct: false },
+      { text: 'Les aider à accéder aux services de santé mentale', correct: false },
+      { text: 'Apprendre et partager des faits, éliminer les mythes si vous êtes au courant', correct: false },
+      { text: 'Tout ce qui précède', correct: true }
     ]
   },
   {
-    question: 'Mental health is not',
+    question: 'La santé mentale n\'est pas',
     answers: [
-      { text: 'A myth', correct: true },
-      { text: 'Normal as Physical health conditions', correct: false },
-      { text: 'Natural behavioral and emotional malfunction', correct: false },
-      { text: 'Something that needs attention and awareness', correct: false }
+      { text: 'Un mythe', correct: true },
+      { text: 'Normale comme les conditions de santé physique', correct: false },
+      { text: 'Un dysfonctionnement comportemental et émotionnel naturel', correct: false },
+      { text: 'Quelque chose qui nécessite une attention et une sensibilisation', correct: false }
     ]
   }
-]
+];
